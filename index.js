0d07a6db7c44aca45f62ef326dd46187336459f9
guang.fan <guang.fan@yiducloud.cn>
2018-11-12 17:21:02 +0800

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+;(function (factory) {
+    var registeredInModuleLoader = false;
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+		registeredInModuleLoader = true;
+	}
+	if (typeof exports === 'object') {
+		module.exports = factory();
+		registeredInModuleLoader = true;
+	}
+	if (!registeredInModuleLoader) {
+		window.vanTool = factory();
+	}
+})(function () {
+    return {
+        getDate: function () {
+
+        }
+    };
+});
